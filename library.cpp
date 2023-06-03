e5beb81b1bc380290f38dfd2d70b10f4a52da3b4
PetrFlajsingr <flajsingr@gmail.com>
2023-06-03 21:44:24 +0200

    Add demo of member fnc creation

--- library.cpp
+++ library.cpp
@@ -6,16 +6,23 @@ using namespace std::string_literals;
 
 class DemoCodeGenerator : public pf::meta_gen::CodeGenerator {
 public:
-    pf::meta_gen::GenerationResult start() override { return {"// Code generated in header\n", "// Code generated in source\n"}; }
+    pf::meta_gen::GenerationResult start() override {
+        return {"", "#include \""s + std::string{getRelativePathToOriginal()} + "\"\n", ""};
+    }
 
     pf::meta_gen::RecordGenerationResult generate(const pf::meta_gen::RecordTypeInfo &typeInfo) override {
-        return {"// "s + typeInfo.fullName + "\n", "// "s + typeInfo.fullName + "\n", "// "s + typeInfo.fullName + "\n"};
+        return {
+                "public: int demo() const;\n",
+                "// "s + typeInfo.fullName + "\n",
+                "// "s + typeInfo.fullName + "\n",
+                "int "s + typeInfo.fullName + "::demo() const { return 10; }\n",
+        };
     }
 
     pf::meta_gen::GenerationResult generate(const pf::meta_gen::EnumTypeInfo &typeInfo) override {
         return {"// "s + typeInfo.fullName + "\n", "// "s + typeInfo.fullName + "\n"};
     }
-    pf::meta_gen::GenerationResult end() override { return {"// Code generated in header\n", "// Code generated in source"}; }
+    pf::meta_gen::GenerationResult end() override { return {"// Code generated in header\n", "// Code generated in source", ""}; }
 
     // decides order in which the generators are used
     uint64_t getPriority() const override { return 10; }
