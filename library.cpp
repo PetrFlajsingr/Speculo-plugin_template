0fb330629467afbac6d9f2321a0f44330cd13abe
PetrFlajsingr <flajsingr@gmail.com>
2023-07-18 12:35:33 +0200

    Update for newer speculo version

--- library.cpp
+++ library.cpp
@@ -6,27 +6,24 @@ using namespace std::string_literals;
 
 class DemoCodeGenerator : public speculo::gen::CodeGenerator {
 public:
-    speculo::gen::GenerationResult start() override {
-        return {"", "#include \""s + std::string{getRelativePathToOriginal()} + "\"\n", ""};
-    }
 
     speculo::gen::RecordGenerationResult generate(speculo::gen::RecordTypeInfo &typeInfo) override {
-        return {
-                "// "s + typeInfo.fullName + "\n",
-                "int "s + typeInfo.fullName + "::demo() const { return 10; }\n",
-                "// "s + typeInfo.fullName + "\n",
-                {},
-                "public: int demo() const;\n"
-        };
+        speculo::gen::RecordGenerationResult result;
+        result.typeBodyCode = "public: int demo() const;\n";
+        result.cppCode = "int "s + typeInfo.fullName + "::demo() const { return 10; }\n";
+        return result;
     }
 
     speculo::gen::GenerationResult generate(const speculo::gen::EnumTypeInfo &typeInfo) override {
         return {"// "s + typeInfo.fullName + "\n", "// "s + typeInfo.fullName + "\n"};
     }
 
-    speculo::gen::GenerationResult end() override {
-        return {"// Code generated in header\n", "// Code generated in source", ""};
+    speculo::gen::FilePrologueEpilogue getPrologueEpilogue() override {
+        return {
+                .cppPrologue = "#include \""s + std::string{getRelativePathToOriginal()} + "\"\n"
+        };
     }
+
 
     // decides order in which the generators are used
     std::uint64_t getPriority() const override { return 10; }
